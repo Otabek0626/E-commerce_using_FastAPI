0f234b5c097d7f2fb086d7689923d248b9cb0d07
Otabek <bekdevs01@gmail.com>
2022-12-25 22:44:06 +0900

    fix

--- app/core/admin/models.py
+++ app/core/admin/models.py
@@ -4,13 +4,3 @@ from sqlalchemy.sql.sqltypes import TIMESTAMP
 from sqlalchemy.orm import relationship
 
 from ..database import Base
-
-
-class Admin(Base):
-    __tablename__ = "admins"
-
-    id = Column(Integer, primary_key = True, nullable = False)
-    email = Column(String, nullable = False, unique = True)
-    password = Column(String, nullable = False)
-    created_at = Column(TIMESTAMP(timezone=False), nullable=False, server_default=text('now()'))
-    phone_number = Column(String, nullable=True, unique=True)
